f496d65f1ca2335cf72a035f6d243367d76cf647
Simon de Hartog <simon@fs2a.pro>
2017-11-07 19:32:19 +0100

    Start with logging macros

--- src/fs2a.h
+++ src/fs2a.h
@@ -4,3 +4,15 @@
 #pragma once
 
 #define UNUSED(expr) (void)(expr)
+
+#include <sys/time.h>
+#include <boost/log/trivial.hpp>
+
+/** Desired log format:
+ * time_t with microseconds
+ * thread ID
+ * the file location relative to the repo basedir
+ * A colon and a line number
+ * Severity in CAPS
+ * Rest of message */
+// #define LD {BOOST_LOG_TRIVIAL(debug) << 
