2353f2d5039a6fb9c1e8180ec0956eaa43149d8a
Simon de Hartog <simon@fs2a.pro>
2017-11-07 16:38:18 +0100

    Code-styled and repaired Singleton

--- src/Singleton.h
+++ src/Singleton.h
@@ -11,47 +11,53 @@ namespace Fs2a {
 	template <class T>
 	class Singleton {
 		protected:
-		/// Internal pointer to instance
-		static std::unique_ptr<T> instance_a;
+			/// Internal pointer to instance
+			static T *instance_a;
 
-		/// Mutex to prevent race conditions upon construction
-		static std::mutex mux_a;
+			/// Mutex to prevent race conditions upon construction
+			static std::mutex mux_a;
 
-		/// Default constructor
-		inline Singleton<T>() {};
+			/// Default constructor
+			inline Singleton<T>() {};
 
-		/// Copy constructor
-		Singleton<T>(const Singleton<T> & obj_i) = delete;
+			/// Copy constructor
+			Singleton<T>(const Singleton<T> & obj_i) = delete;
 
-		/// Assignment constructor
-		Singleton<T> & operator=(const Singleton<T> & obj_i) = delete;
+			/// Assignment constructor
+			Singleton<T> & operator=(const Singleton<T> & obj_i) = delete;
 
-		/// Destructor
-		~Singleton<T>();
+			/// Destructor
+			~Singleton<T>();
 
 		public:
-		/** Get the Singleton instance pointer.
-		 * @returns a pointer to the singleton instance. */
-		static inline T *instance() {
-			std::lock_guard<std::mutex> lck(mux_a);
-			if (!instance_a) instance_a.reset(new T());
-			return instance_a.get();
-		}
+			/** Get the Singleton instance pointer.
+			 * @returns a pointer to the singleton instance. */
+			static inline T *instance()
+			{
+				std::lock_guard<std::mutex> lck(mux_a);
+				if (instance_a == nullptr) instance_a = new T();
+				return instance_a;
+			}
 
-		/** Explicitly close the singleton */
-		static inline void close() {
-			std::lock_guard<std::mutex> lck(mux_a);
-			instance_a.reset(nullptr);
-		}
+			/** Explicitly close the singleton */
+			static inline void close()
+			{
+				std::lock_guard<std::mutex> lck(mux_a);
+				if (instance_a != nullptr) {
+					delete instance_a;
+					instance_a = nullptr;
+				}
+			}
 
-		static inline bool is_constructed() {
-			std::lock_guard<std::mutex> lck(mux_a);
-			return (bool) instance_a;
-		}
+			static inline bool is_constructed()
+			{
+				std::lock_guard<std::mutex> lck(mux_a);
+				return instance_a != nullptr;
+			}
 
 	};
 
-	template <class T> std::unique_ptr<T> Singleton<T>::instance_a;
+	template <class T> T *(Singleton<T>::instance_a) = nullptr;
 	template <class T> std::mutex Singleton<T>::mux_a;
 
 } // Fs2a namespace
