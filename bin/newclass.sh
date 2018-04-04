6bc9baff829e33ace762ff82f3956b76bced2411
Simon de Hartog <simon@fs2a.pro>
2018-04-04 10:05:31 +0200

    Add namespace to class generator

--- bin/newclass.sh
+++ bin/newclass.sh
@@ -6,37 +6,48 @@ then
 	exit 1
 fi
 
-EMAIL="simon@fs2a.pro"
+email="simon@fs2a.pro"
 if pwd | grep -q 'hyn'
 then
-	COPY="Hear-Your-News LLC"
+	copy="Hear-Your-News LLC"
+	ns="HYN"
 else
-	COPY="Fs2a Ltd."
+	copy="Fs2a Ltd."
+	ns="Fs2a"
 fi
-COPY="${COPY} (c) `date +%Y`"
+copy="${copy} (c) `date +%Y`"
 
 cat > $1.h << EOF
-/** @author   Simon de Hartog <$EMAIL>
- * @copyright $COPY
+/** @author   Simon de Hartog <$email>
+ * @copyright $copy
  * vim:set ts=4 sw=4 noexpandtab: */
 
 #pragma once
 
-class $1
-{
-	protected:
+namespace $ns {
 
-	public:
-	$1();
-	~$1();
-};
+	class $1
+	{
+		protected:
+
+		public:
+		$1();
+		~$1();
+	};
+
+} // $ns namespace
 EOF
 
 cat > $1.cpp << EOF
-/** @author   Simon de Hartog <$EMAIL>
- * @copyright $COPY
+/** @author   Simon de Hartog <$email>
+ * @copyright $copy
  * vim:set ts=4 sw=4 noexpandtab: */
 
 #include "$1.h"
 
+namespace $ns {
+
+
+
+} // $ns namespace
 EOF
