cc8cbcc95592d5076f42f165c9971e1079915f3e
Simon de Hartog <simon@fs2a.pro>
2022-05-02 17:32:18 +0200

    WIP on rewrite to shared ptrs

--- src/Observable.h
+++ src/Observable.h
@@ -1,66 +1,78 @@
-/* Copyright (c) 2020 Simon de Hartog <simon@dehartog.name>
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its
-contributors may be used to endorse or promote products derived from this
-software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-vim:set ts=4 sw=4 noexpandtab: */
+/* vim:set ts=4 sw=4 noet tw=90:
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020-2022, Fs2a, Simon de Hartog <simon@fs2a.pro>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 #pragma once
 
-#include <atomic>
 #include <mutex>
-#include <set>
+#include <unordered_set>
 #include "Observer.h"
 
 namespace Fs2a {
-
-	// Forward declaration to prevent inclusion loops
-	template <class T>
-	class Observer;
-
+	/** Keep track of observers that are observing us and notify them when
+	 * the observed value changes. Everything is administered through weak
+	 * pointers to prevent destruction deadlocks and shared pointer loops.
+	 */
 	template <class T>
 	class Observable
 	{
-		// Allow Observers to add and remove themselves.
-		friend class Observer<T>;
-
 		private:
 			// Mutex to prevent race conditions
-			std::mutex observableMux_a;
+			std::mutex mux_;
 
 			// Set of observers
-			std::set<Observer<T> *> observers_a;
+			std::unordered_set<std::weak_ptr<Observer<T>>> observers_;
 
 			// Keep track of the actual value
-			std::atomic<T> value_a;
+			T value_;
+
+			/** Safety method that gets the weak shared pointer from the
+			 * given observer to prevent segfaults when propagating changes
+			 * to the observed value.
+			 * @returns False when not actually registered, true when it
+			 * is. */
+			bool addObserver(Observer<T> * observer_i) {
+				if (observer_i == nullptr) return false;
+				return addObserver(observer_i->weak_from_this());
+			}
 
 			/** Add an observer to this observable to receive future value updates.
-			 * @param observerI Observer that will be observing us.
-			 * @returns True if @p observerI is newly observing us, false if
-			 * it was already there. */
-			bool addObserver(Observer<T> *observerI) {
-				std::lock_guard<std::mutex> lg(observableMux_a);
-				auto p = observers_a.insert(observerI);
+			 * @param observer_i Weak pointer to Observer that will be observing us.
+			 * @returns True if @p observer_i is newly observing us, false if
+			 * it was already there or if the given @p observer_i weak
+			 * pointer has expired. */
+			bool addObserver(std::weak_ptr<Observer<T>> observer_i) {
+				if (observer_i.expired()) return false;
+				std::lock_guard<std::mutex> lg(mux_a);
+				auto p = observers_.insert(observer_i);
 				return p.second;
 			}
 
@@ -69,34 +81,58 @@ namespace Fs2a {
 			 * @returns True if @p observerI was removed, false if
 			 * it was not present in the first place. */
 			bool removeObserver(Observer<T> *observerI) {
-				std::lock_guard<std::mutex> lg(observableMux_a);
+				std::lock_guard<std::mutex> lg(mux_a);
 				return observers_a.erase(observerI) > 0;
 			}
 
 		public:
 
 			// Constructor to initialize internal value
-			Observable(const T & valueI)
-				: value_a(valueI) {}
+			Observable(const T & value_i)
+				: value_(value_i) {}
 
 			// Destructor
 			~Observable() {}
 
 			// Retrieve the value
-			const T get() const { return value_a.load(); }
+			T get() const { return value_; }
 
 			/** Get the current list of observers. */
-			const std::set<Observer<T> *> & observers() const { return observers_a; }
+			const std::unordered_set<std::weak_ptr<Observer<T>>> & observers() const { return observers_; }
 
 			/** Set the value and broadcast it if it actually changed.
-			 * @param valueI The new value to set. */
-			void set(const T & valueI) {
-				if (valueI == value_a) return;
-				std::lock_guard<std::mutex> lg(observableMux_a);
-				value_a.store(valueI);
-				for (auto const & i: observers_a) {
-					i->update(value_a.load());
+			 * @param value_i The new value to set.
+			 * @returns True if value was changed and broadcasted to
+			 * observers, false if value is unchanged. */
+			bool set(const T & value_i) {
+				std::unique_lock<std::mutex> lck(mux_);
+				if (value_i == value_) return false;
+				value_ = value_i;
+
+				/** Make a deep copy of all the Observers To Be Notified so
+				 * we can unlock the mutex to prevent deadlocks. */
+				std::unordered_set<std::weak_ptr<Observer<T>>> otbn = observers_;
+				lck.unlock();
+				bool foundExpired = false;
+				for (std::weak_ptr<Observer<T>> i: otbn) {
+					// Shared Pointer to Observer
+					auto spo = i.lock();
+					if (!spo) {
+						foundExpired = true;
+						continue;
+					}
+					spo->observableChanged(value_);
 				}
+
+				if (foundExpired) {
+					lck.lock();
+					for (auto it = observers_.begin(); it != observers_.end(); ) {
+						if (it->expired()) it = observers_.erase(it);
+						else it++;
+					}
+				}
+
+				return true;
 			}
 
 	};
--- src/Observer.h
+++ src/Observer.h
@@ -1,97 +1,51 @@
-/* Copyright (c) 2020 Simon de Hartog <simon@dehartog.name>
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its
-contributors may be used to endorse or promote products derived from this
-software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-vim:set ts=4 sw=4 noexpandtab: */
+/* vim:set ts=4 sw=4 noet tw=90:
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2020-2022, Fs2a, Simon de Hartog <simon@fs2a.pro>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 #pragma once
 
-#include <mutex>
-#include "Observable.h"
-
 namespace Fs2a {
-
+	/** Class that can observe an Observable object. Just forces the
+	 * implementation of the observableChanged() method. No administration
+	 * of the observed object is done. */
 	template <class T>
-	class Observable;
-
-	template <class T>
-	class Observer
+	class Observer : enable_shared_from_this<Observer>
 	{
-		private:
-			// Mutex to prevent race conditions
-			std::mutex observerMux_a;
-
-			// Observable we are observing
-			Observable<T> *observable_a;
-
 		public:
-			/** Observer constructor. */
-			Observer() : observable_a(nullptr) {}
-
-			/** Observer destructor. Unsubscribes from Observable if
-			 * previously subscribed. */
-			~Observer() {
-				observe(nullptr); // Unsubscribe if previously subscribed
-			}
-
-			/** Get the current observable object being observed.
-			 * @returns A pointer to the observed object,
-			 * NULL if no object is being observed. */
-			Observable<T> * observable() const { return observable_a; }
-
-			/** Start observing an Observable object. Calling this method
-			 * ensures ignoring the object again when this class is destroyed, which
-			 * will prevent segmentation faults.
-			 * @param observableI Observable to start observing. If
-			 * observableI is NULL, this method starts ignoring the object if previously
-			 * observing. When another object was previously being
-			 * observed, that object will start being ignored.
-			 * @returns True if the observable object was changed (both in
-			 * this object and in the observable object), false if not. */
-			bool observe(Observable<T> *observableI = nullptr) {
-				std::lock_guard<std::mutex> lg(observerMux_a);
-				if (observableI == nullptr) {
-					if (observable_a == nullptr) return false;
-					bool rv = observable_a->removeObserver(this);
-					observable_a = nullptr;
-					return rv;
-				} else {
-					if (observableI == observable_a) return false;
-					if (observable_a != nullptr) {
-						return observable_a->removeObserver(this);
-					}
-					observable_a = observableI;
-					return observable_a->addObserver(this);
-				}
-			}
-
-			/** Called when the value in the Observable class is updated.
+			/** Called when the value in the Observable class is changed.
 			 * Please do only little processing here, since this method is
 			 * called from the Observable class thread that updated the
 			 * value.
-			 * @param valueI The new value that was set. */
-			virtual void update(const T & valueI) = 0;
+			 * @param value_i The new value that was set. */
+			virtual void observableChanged(const T & value_i) = 0;
 	};
 
 } // Fs2a namespace
