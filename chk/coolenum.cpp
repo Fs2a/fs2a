15e05b7ae8d1d75676fd859d24fae7690ed56e50
Bren de Hartog <bren@dehartog.name>
2025-08-14 19:15:13 +0200

    test(coolenum): Started tests for CoolEnum

--- chk/coolenum.cpp
+++ chk/coolenum.cpp
@@ -27,43 +27,33 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-#include <stdexcept>
-#include <string>
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
 
-#ifdef COOLENUM
-#undef COOLENUM_BEGIN
-#undef COOLENUM
-#undef COOLENUM_END
-#endif
+#include <fs2a/CoolEnum.h.h>
+#include "coolenum.def"
+#include <fs2a/CoolEnum.cpp.h>
+#include "coolenum.def"
 
-#define COOLENUM_BEGIN(NAME, KEYTYPE) \
-	const std::string & NAME ## _ (const KEYTYPE p_key) { \
-		NAME ## _fillenummaps(); \
-		auto i = NAME ## _k2s.find(p_key); \
-		if (i == NAME ## _k2s.end()) { \
-			throw std::out_of_range("Requested enum key not found in " #NAME); \
-		} \
-		return i->second; \
-	} \
-	NAME NAME ## _ (const std::string & p_str) { \
-		NAME ## _fillenummaps(); \
-		auto i = NAME ## _s2k.find(p_str); \
-		if (i == NAME ## _s2k.end()) { \
-			throw std::out_of_range("Requested enum value not found in " #NAME); \
-		} \
-		return (NAME) i->second; \
-	} \
-	std::unordered_map<KEYTYPE, std::string> NAME ## _k2s; \
-	std::unordered_map<std::string, KEYTYPE> NAME ## _s2k; \
-	void NAME ## _fillenummaps() { \
-		std::unordered_map<KEYTYPE, std::string> * k2s = & NAME ## _k2s; \
-		std::unordered_map<std::string, KEYTYPE> * s2k = & NAME ## _s2k; \
-		if (k2s->empty()) {
+#define CHECKNAME CoolEnumCheck
 
-#define COOLENUM(VALUE, INDEX) \
-	(*k2s)[INDEX] = #VALUE; \
-	(*s2k)[#VALUE] = INDEX;
+class CHECKNAME;
 
-#define COOLENUM_END() \
-	} \
+CPPUNIT_TEST_SUITE_REGISTRATION(CHECKNAME);
+
+class CHECKNAME : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(CHECKNAME);
+	CPPUNIT_TEST(basics);
+	CPPUNIT_TEST_SUITE_END();
+
+	public:
+	void basics()
+	{
+		CPPUNIT_ASSERT_EQUAL(0, plain_k2s.size());
+		CPPUNIT_ASSERT_EQUAL(0, plain_s2k.size());
+
 	}
+
+};
+
+#undef CHECKNAME
