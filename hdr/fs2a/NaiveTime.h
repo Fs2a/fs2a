ccfd147e9754354b76a44db3ee6a7f5007314139
Bren de Hartog <bren@fs2a.pro>
2025-08-30 15:37:00 +0200

    feat(NaiveTime): Started class def

---
+++ hdr/fs2a/NaiveTime.h
@@ -0,0 +1,54 @@
+/** @author   Bren de Hartog <bren@fs2a.pro>
+ * @copyright Copyright (c) 2025, Bren de Hartog. All rights reserved.
+ * @license   This project is licensed under 3-clause BSD license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+#pragma once
+
+#include <cstdint>
+
+namespace Fs2a {
+
+	/** Class to administer a time of day with hours and minutes.
+	 * No seconds are taken into account and a 24-hour clock is used for representation. */
+	class NaiveTime {
+		/// Hour, sentinel value 0xFF
+		uint8_t hour_;
+
+		/// Minute, sentinel value 0xFF
+		uint8_t min_;
+
+		public:
+		/// Default constructor
+		NaiveTime();
+
+		/// Default destructor
+		~NaiveTime() = default;
+
+	}; // NaiveTime class
+
+} // Fs2a namespace
